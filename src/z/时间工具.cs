397164f58fcad6e72c7d8789832a8207591f0035
Miltomo <1523208477@qq.com>
2024-01-20 22:38:03 +0800

    AF TimeTool.FormatMS()

---
+++ src/z/时间工具.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MHTools
+{
+    public static class TimeTool
+    {
+        public static string FormatMS(int milliseconds, bool inEnglish = false)
+        {
+            TimeSpan timeSpan = TimeSpan.FromMilliseconds(milliseconds);
+
+            int hours = timeSpan.Hours;
+            int minutes = timeSpan.Minutes;
+            int seconds = timeSpan.Seconds;
+
+            string f = $"{seconds}{(inEnglish ? "s" : "秒")}";
+            if (minutes > 0)
+                f = $"{minutes}{(inEnglish ? "m" : "分")}" + f;
+            if (hours > 0)
+                f = $"{hours}{(inEnglish ? "h" : "时")}" + f;
+
+            return f;
+        }
+    }
+}
