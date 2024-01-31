6d81669f8ccfae20896579b1f0dd499fc67da745
Miltomo <1523208477@qq.com>
2024-01-31 21:12:40 +0800

    A 过程信息文件

---
+++ src/PText.cs
@@ -0,0 +1,27 @@
+﻿namespace Shining_BeautifulGirls
+{
+    /// <summary>
+    /// 过程信息文本
+    /// </summary>
+    static class PText
+    {
+        /// <summary>
+        /// 群英联赛
+        /// </summary>
+        public static class Extravaganza
+        {
+            public static string 决赛 => nameof(决赛);
+        }
+
+        /// <summary>
+        /// (养成中的)比赛流程
+        /// </summary>
+        public static class Race
+        {
+            public static string 赛事详情 => nameof(赛事详情);
+            public static string 前往赛事 => nameof(前往赛事);
+            public static string 重新挑战 => nameof(重新挑战);
+            public static string 下一页 => nameof(下一页);
+        }
+    }
+}
