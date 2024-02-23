7b3f45208fbb243cfc7c937650ba1e6623491a24
Miltomo <1523208477@qq.com>
2024-02-23 23:45:26 +0800

    M 更新资源文件

--- src/Symbol.cs
+++ src/Symbol.cs
@@ -4,39 +4,27 @@ namespace Shining_BeautifulGirls
 {
     static class Symbol
     {
-        public static string OK => Path.Combine(World.SymbolDir, "OK.png");
         public static string 下一页 => Path.Combine(World.SymbolDir, "下一页.png");
-        public static string 主界面 => Path.Combine(World.SymbolDir, "主界面.png");
         public static string 养成 => Path.Combine(World.SymbolDir, "养成.png");
         public static string 养成2 => Path.Combine(World.SymbolDir, "养成2.png");
-        public static string 养成主页 => Path.Combine(World.SymbolDir, "养成主页.png");
-        public static string 养成结束 => Path.Combine(World.SymbolDir, "养成结束.png");
         public static string 决定 => Path.Combine(World.SymbolDir, "决定.png");
-        public static string 决赛 => Path.Combine(World.SymbolDir, "决赛.png");
         public static string 回复选条 => Path.Combine(World.SymbolDir, "回复选条.png");
-        public static string 因子继承 => Path.Combine(World.SymbolDir, "因子继承.png");
         public static string 夏日 => Path.Combine(World.SymbolDir, "夏日.png");
         public static string 多次参赛ON => Path.Combine(World.SymbolDir, "多次参赛ON.png");
         public static string 已获得 => Path.Combine(World.SymbolDir, "已获得.png");
         public static string 快进 => Path.Combine(World.SymbolDir, "快进.png");
-        public static string 技白 => Path.Combine(World.SymbolDir, "技白.png");
         public static string 技能加 => Path.Combine(World.SymbolDir, "技能加.png");
-        public static string 技能获取 => Path.Combine(World.SymbolDir, "技能获取.png");
         public static string 抓娃娃 => Path.Combine(World.SymbolDir, "抓娃娃.png");
-        public static string 无法参赛 => Path.Combine(World.SymbolDir, "无法参赛.png");
-        public static string 未达要求 => Path.Combine(World.SymbolDir, "未达要求.png");
+        public static string 普通日主页 => Path.Combine(World.SymbolDir, "普通日主页.png");
         public static string 未选继承 => Path.Combine(World.SymbolDir, "未选继承.png");
         public static string 比赛日主页 => Path.Combine(World.SymbolDir, "比赛日主页.png");
-        public static string 粉丝不足 => Path.Combine(World.SymbolDir, "粉丝不足.png");
         public static string 继续 => Path.Combine(World.SymbolDir, "继续.png");
-        public static string 缩短事件 => Path.Combine(World.SymbolDir, "缩短事件.png");
         public static string 能量饮料 => Path.Combine(World.SymbolDir, "能量饮料.png");
-        public static string 赛事 => Path.Combine(World.SymbolDir, "赛事.png");
         public static string 赛事活动 => Path.Combine(World.SymbolDir, "赛事活动.png");
+        public static string 赛事结束 => Path.Combine(World.SymbolDir, "赛事结束.png");
         public static string 赛果 => Path.Combine(World.SymbolDir, "赛果.png");
         public static string 返回 => Path.Combine(World.SymbolDir, "返回.png");
         public static string 金币 => Path.Combine(World.SymbolDir, "金币.png");
-        public static string 预测 => Path.Combine(World.SymbolDir, "预测.png");
     }
 }
 
