828084a8238f3e2593d54c4431beacbc67d81c76
Miltomo <1523208477@qq.com>
2024-03-28 18:03:49 +0800

    A 抽象FileListBox类: 集成文件管理功能的特殊ListBox

---
+++ src/z/UI/FileListBox.cs
@@ -0,0 +1,116 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace MHTools.UI
+{
+    public class FileListBox
+    {
+        public ListBox Main { get; init; }
+        public SimpleFileManager FileManager { get; init; }
+        Dispatcher Dispatcher => Main.Dispatcher;
+
+        TextBlock? textBlockInEdit;
+        TextBox? textBoxInEdit;
+
+        //===============================================================
+        private void SwitchMode(bool edit = true)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                if (textBlockInEdit != null && textBoxInEdit != null)
+                {
+                    var dqFileName = edit ?
+                        textBlockInEdit.Text :
+                        textBoxInEdit.Text;
+
+                    textBoxInEdit.IsEnabled = edit;
+                    textBlockInEdit.Visibility = edit ? Visibility.Hidden : Visibility.Visible;
+                    textBoxInEdit.Visibility = edit ? Visibility.Visible : Visibility.Hidden;
+
+                    if (edit)
+                    {
+                        textBoxInEdit.Text = dqFileName;
+                        textBoxInEdit.Focus();
+                        textBoxInEdit.CaretIndex = dqFileName.Length;
+                    }
+                    else
+                    {
+                        if (FileManager.TryRename(dqFileName))
+                            Main.ItemsSource = FileManager.Names;
+                        textBlockInEdit = null;
+                        textBoxInEdit = null;
+                    }
+                }
+            });
+        }
+
+        public FileListBox(ListBox target, SimpleFileManager fileManager)
+        {
+            FileManager = fileManager;
+            Main = target;
+            Main.ItemsSource = FileManager.Names;
+        }
+
+        public void EnterRename(object? item = null)
+        {
+            if (FileManager.SelectedFile is null)
+                return;
+
+            object source = item ?? Main.ItemContainerGenerator.ContainerFromItem(Main.SelectedItem);
+
+            // 获取目标项
+            ListBoxItem? listBoxItem = VET.FindVisualParent<ListBoxItem>(source as DependencyObject);
+
+
+
+            if (listBoxItem != null)
+            {
+                // 获取项中的元素
+                textBlockInEdit = VET.FindVisualChild<TextBlock>(listBoxItem);
+                textBoxInEdit = VET.FindVisualChild<TextBox>(listBoxItem);
+
+                SwitchMode();
+            }
+        }
+
+        public void EndRename() => SwitchMode(false);
+
+        /// <summary>
+        /// 新建列表项（新建文件）
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <returns>新文件的绝对路径</returns>
+        public string Add(FrameworkElement? sender = null)
+        {
+            if (sender != null)
+                sender.IsEnabled = false;
+            string fileName = FileManager.Add();
+            Dispatcher.Invoke(() =>
+            {
+                Main.ItemsSource = FileManager.Names;
+                if (sender != null)
+                    sender.IsEnabled = true;
+            });
+            return fileName;
+        }
+
+        /// <summary>
+        /// 删除所选项(代表的文件)
+        /// </summary>
+        public void Delete()
+        {
+            if (FileManager.SelectedFile != null)
+            {
+                FileManager.Delete();
+                Dispatcher.Invoke(() =>
+                {
+                    Main.ItemsSource = FileManager.Names;
+                });
+            }
+        }
+
+        public bool Select(string name) => FileManager.Select(name);
+        public void Cancel() => FileManager.CancelSelect();
+    }
+}
---
+++ src/z/UI/VisualTree/VET.cs
@@ -0,0 +1,147 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace MHTools.UI
+{
+    public static class VET
+    {
+        public static ScrollViewer? GetScrollViewer(DependencyObject element)
+        {
+            if (element is ScrollViewer scrollViewer)
+            {
+                return scrollViewer;
+            }
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++)
+            {
+                var child = VisualTreeHelper.GetChild(element, i);
+
+                var result = GetScrollViewer(child);
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+
+            return null;
+        }
+
+        public static T? FindVisualParent<T>(DependencyObject? child) where T : DependencyObject
+        {
+            while (child != null)
+            {
+                if (child is T parent)
+                {
+                    return parent;
+                }
+                child = VisualTreeHelper.GetParent(child);
+            }
+            return null;
+        }
+
+        public static T? FindVisualChild<T>(DependencyObject parent) where T : DependencyObject
+        {
+            int count = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < count; i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+
+                if (child is not null)
+                {
+                    if (child is T target)
+                        return target;
+                    else
+                    {
+                        var childOfChild = FindVisualChild<T>(child);
+                        if (childOfChild != null)
+                            return childOfChild;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static void SetImage(Image image, string? picturePath)
+        {
+            if (string.IsNullOrEmpty(picturePath))
+                return;
+
+            // 创建BitmapImage对象
+            BitmapImage bitmap = new();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(picturePath, UriKind.RelativeOrAbsolute);
+            bitmap.EndInit();
+
+            // 将BitmapImage对象分配给Image控件的Source属性
+            image.Source = bitmap;
+        }
+
+        // 递归函数，检查指定控件及其子控件是否包含指定文本的 TextBlock
+        public static bool CheckForTextBlock(DependencyObject parent, string searchText)
+        {
+            int count = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < count; i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+                if (child != null)
+                {
+                    if (child is TextBlock textBlock)
+                    {
+                        if (textBlock.Text == searchText)
+                        {
+                            // 找到了指定文本的 TextBlock，返回 true
+                            return true;
+                        }
+                    }
+
+                    // 递归调用，继续检查子控件
+                    if (CheckForTextBlock(child, searchText))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        // 获取指定控件及其子控件中所有 TextBlock 的文本
+        public static string[] GetAllTextBlockTexts(DependencyObject parent)
+        {
+            List<string> texts = [];
+            int count = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < count; i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+                if (child != null)
+                {
+                    if (child is TextBlock textBlock)
+                    {
+                        texts.Add(textBlock.Text);
+                    }
+
+                    // 递归调用，继续检查子控件
+                    string[] childTexts = GetAllTextBlockTexts(child);
+                    texts.AddRange(childTexts);
+                }
+            }
+            return [.. texts];
+        }
+
+        public static List<T> Items2List<T>(ItemCollection itemCollection)
+        {
+            List<T> itemList = [];
+
+            foreach (var item in itemCollection)
+            {
+                if (item is T convertedItem)
+                {
+                    itemList.Add(convertedItem);
+                }
+            }
+
+            return itemList;
+        }
+    }
+}
