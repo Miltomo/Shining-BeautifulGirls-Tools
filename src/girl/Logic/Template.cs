10b7c228b5dd5fa87c2844288c0b8537d431339d
Miltomo <1523208477@qq.com>
2024-04-25 13:18:23 +0800

    A Template.cs

---
+++ src/girl/Logic/Template.cs
@@ -0,0 +1,25 @@
+﻿namespace Shining_BeautifulGirls
+{
+    partial class ShiningGirl
+    {
+        class YourAlgorithm : Algorithm
+        {
+            // 构造函数
+            public YourAlgorithm(ShiningGirl girl) : base(girl)
+            {
+            }
+
+            // 实现你的打分逻辑
+            protected override double Score(Plan info)
+            {
+                throw new NotImplementedException();
+            }
+
+            // 实现你的选择逻辑
+            protected override Plan Select()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
